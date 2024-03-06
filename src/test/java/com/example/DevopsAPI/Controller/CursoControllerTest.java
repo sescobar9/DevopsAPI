f7c6674c311fb2bf89f05256f76c8a5e8dc64083
sescobar9 <santiagoesco99@gmail.com>
2024-03-05 23:36:59 -0500

    cambios de configuraciones

---
+++ src/test/java/com/example/DevopsAPI/Controller/CursoControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.DevopsAPI.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CursoControllerTest {
+
+    @Test
+    void cursos() {
+    }
+
+    @Test
+    void guardarCurso() {
+    }
+
+    @Test
+    void actualizarCurso() {
+    }
+
+    @Test
+    void eliminarCurso() {
+    }
+}
---
+++ src/test/java/com/example/DevopsAPI/Controller/ProfesorControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.DevopsAPI.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfesorControllerTest {
+
+    @Test
+    void getProfesores() {
+    }
+
+    @Test
+    void guardarProfesor() {
+    }
+
+    @Test
+    void actualizarProfesor() {
+    }
+
+    @Test
+    void eliminarProfesor() {
+    }
+
+    @Test
+    void asociarProfesorConCurso() {
+    }
+}
