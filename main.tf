146a0e993eb5d0f09669e2291ca5bb2f7239f815
sescobar9 <santiagoesco99@gmail.com>
2024-06-06 16:48:20 -0500

    terraform

--- main.tf
+++ main.tf
@@ -4,20 +4,20 @@ terraform {
   required_version = ">= 0.14"
 
   required_providers {
+    # Cloud Run support was added on 3.3.0
     google = ">= 3.3"
   }
 }
-
 provider "google" {
-  project = "springgcp-425215"
+  # Replace `PROJECT_ID` with your project
+  project = "apidevops123"
 }
-
+# Enables the Cloud Run API
 resource "google_project_service" "run_api" {
   service = "run.googleapis.com"
 
   disable_on_destroy = true
 }
-
 # Create the Cloud Run service
 resource "google_cloud_run_service" "run_service" {
   name = "app"
@@ -26,7 +26,7 @@ resource "google_cloud_run_service" "run_service" {
   template {
     spec {
       containers {
-        image = "asia.gcr.io/springgcp-425215/app-engine-tmp/app/default/ttl-18h"
+        image = "gcr.io/google-samples/hello-app:1.0"
       }
     }
   }
@@ -36,14 +36,17 @@ resource "google_cloud_run_service" "run_service" {
     latest_revision = true
   }
 
+  # Waits for the Cloud Run API to be enabled
   depends_on = [google_project_service.run_api]
 }
+# Allow unauthenticated users to invoke the service
 resource "google_cloud_run_service_iam_member" "run_all_users" {
   service  = google_cloud_run_service.run_service.name
   location = google_cloud_run_service.run_service.location
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
+# Display the service URL
 output "service_url" {
   value = google_cloud_run_service.run_service.status[0].url
 }
